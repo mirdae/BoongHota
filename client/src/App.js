b9da9b16a35ca16564d0a28225900a2d5495f0bb
hyewon do <tsc03018@gmail.com>
2020-12-15 09:40:28 +0900

    [#12] feat : 정적인 상태의 landing컴포넌트 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import './App.scss';
+import LandingPage from './components/LandingPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>어디있니 붕호타</h2>
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/FoodIcons/Ta.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Ta = () => {
+  return (
+    <div className="ta">
+      <div className="ta-icon icon"></div>
+    </div>
+  );
+};
+
+export default Ta;
