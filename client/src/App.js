881b0d1130a8ccdddd0326743b5407c9414b5f40
hyewon do <tsc03018@gmail.com>
2020-12-17 15:41:09 +0900

    [#23] env : react-router-dom을 사용해서 경로에 맞는 페이지 보여주기

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import MapPage from './components/MapPage/Container';
 
 function App() {
   return (
     <div className="App">
-      <h2>어디있니 붕호타</h2>
-      <MapPage />
+      <Router>
+        <h2>어디있니 붕호타</h2>
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/map/:id" component={MapPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
