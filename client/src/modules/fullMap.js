28103fdc7c2a16176f430872e9e3d902ff1b9d72
hyewon do <tsc03018@gmail.com>
2020-12-27 18:55:42 +0900

    change .gitignore

--- client/src/modules/fullMap.js
+++
@@ -1,8 +0,0 @@
-import { createAction, handleActions } from 'redux-actions';
-import { takeLatest, takeEvery } from 'redux-saga/effects';
-
-export function* fullMapSaga() {}
-
-const initialState = {};
-
-export const fullMap = handleActions({}, initialState);
