35302331da385ca36956271d53896f230834a098
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2020-12-28 20:44:15 +0900

    chore : 로고 이미지에 alt 프로퍼티 추가

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,10 @@ function App() {
       <Router>
         <header>
           <Link to="/">
-            <img src="https://media.vlpt.us/images/dolarge/post/0f4e3ed7-c07c-4e48-afea-dba71b3b306b/logo.png" />
+            <img
+              src="https://media.vlpt.us/images/dolarge/post/0f4e3ed7-c07c-4e48-afea-dba71b3b306b/logo.png"
+              alt="logo"
+            />
           </Link>
         </header>
         <Switch>
