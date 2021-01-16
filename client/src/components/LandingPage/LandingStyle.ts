a8287c271708905ea939f4e42b3b0631da9a85ec
dohye1 <tsc03018@gmail.com>
2021-01-16 14:11:14 +0900

    LandingPage 컴포넌트의 스타일을 scss에서 styled-componenet로 마이그레이션

---
+++ client/src/components/LandingPage/LandingStyle.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import Button from '../common/Button';
+import { darken } from 'polished';
+
+export const LandingContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Main = styled.main`
+  width: 80%;
+  display: flex;
+  & a {
+    display: flex;
+    justify-content: center;
+    width: 33%;
+  }
+`;
+
+export const OpenModalButton = styled(Button)`
+  background-color: #e5b589;
+  &:hover {
+    background-color: ${darken(0.1, '#e5b589')};
+  }
+`;
