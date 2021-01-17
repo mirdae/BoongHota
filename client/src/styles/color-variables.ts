896f9179d7dd5ea052ce23a199c9aad6ec412103
dohye1 <tsc03018@gmail.com>
2021-01-17 14:00:33 +0900

    style과 관련된 값들을 styles라는 폴더를 만들어서 관리함

---
+++ client/src/styles/color-variables.ts
@@ -0,0 +1,15 @@
+type Palette = {
+  BASE: string;
+  POINT: string;
+  BUTTON: string[];
+  MAP_BUTTON: string[];
+};
+
+const PALETTE: Palette = {
+  BASE: '#E6DCD7',
+  POINT: '#DEB089',
+  BUTTON: ['#DEB089', '#988989'],
+  MAP_BUTTON: ['#e4706c', '#4969da'],
+};
+
+export default PALETTE;
---
+++ client/src/styles/img.ts
@@ -0,0 +1,6 @@
+export const BOONG_IMG =
+  'https://media.vlpt.us/images/dolarge/post/9df7aa9c-5827-4928-8711-25763612cc5f/%EB%B6%95%EC%96%B4.png';
+export const HO_IMG =
+  'https://media.vlpt.us/images/dolarge/post/18a0d072-1987-44e6-a8dc-74fb5d40a337/%ED%98%B8%EB%96%A1.png';
+export const TA_IMG =
+  'https://media.vlpt.us/images/dolarge/post/9afdecdf-2a14-4079-9c06-487d657c6c7e/%ED%83%80%EC%BD%94.png';
